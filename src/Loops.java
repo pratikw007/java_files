7e5053271183c7c9ba23937d07de1b33fa0ba76e
Pratik Waghmare <waghmarepratik287@gmail.com>
2022-08-28 11:18:44 +0530

    java

--- src/Loops.java
+++ src/Loops.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+  import java.util.Scanner;
 public class Loops {
     public static void main(String[] args) {
           //Q: Print Number from 1 to 5
@@ -8,8 +10,19 @@ public class Loops {
 
          //body
        */
-        for (int num = 1; num <= 5; num += 1){
-            System.out.println(num );
-        }
-    }
+//        for (int num = 0; num <= 5; num += 1){
+//            System.out.println(num);
+//        }
+        // Q.1: Print a number from one to 20 which should be multiple o 2
+//        for (int num = 0; num <= 20; num += 2){
+//            System.out.println(num);
+//        }
+
+        //print a num from 1 to n
+        Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            for (int num = 1;num <= n; num++)                  {
+                System.out.print(num +" ");
+}
+}
 }
